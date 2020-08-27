361563d2ee1ccfc24432d3b9cf55fb337d7ff508
dessteyu <bayembye10@gmail.com>
2020-08-27 12:38:35 +0000

    update before push

--- src/stories/table/index.stories.tsx
+++ src/stories/table/index.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { CustomTable, TableProps, Column } from "./table";
 export default {
-  title: "Paps/CustomTable",
+  title: "Dessteyu/CustomTable",
   component: CustomTable,
   argTypes: {},
 } as Meta;
